86084acaf3aa8758df61169b63619ed6329e4392
prasad-bigdp <bigdp585@gmail.com>
2024-07-30 09:22:36 +0530

    update

---
+++ JS/code/first/script.js
@@ -0,0 +1,17 @@
+console.log("bye world")
+// single line comments
+/* multi line comment */
+var a1 = 20;
+var a1 = 30; /* redeclare is possible */
+console.log(a1)
+a1 = 50 // reassign is also possible
+let a2 = 30;
+//let a2 = 40; // redclaring is not possible
+a2 = 55; // reassign is possible 
+const a3 = 55;
+a3 = 65; // reassigning is also not possible
+
+// var is a function scoped variable
+// let and const are block variables
+// var can do Hoisting
+// let and const can't
