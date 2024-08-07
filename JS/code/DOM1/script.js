dfc6ce55724fd7395027d485fd297e663ab204f4
prasad-bigdp <bigdp585@gmail.com>
2024-08-07 08:41:00 +0530

    update

---
+++ JS/code/DOM1/script.js
@@ -0,0 +1,7 @@
+console.log(document)
+let x = document.getElementsByTagName('p') // selected paragraphs using tag name
+let y = document.getElementsByClassName('para')
+let z = document.getElementById('pa')
+console.log(z.textContent)
+console.log(y[0].textContent)
+console.log(x[1].textContent)
