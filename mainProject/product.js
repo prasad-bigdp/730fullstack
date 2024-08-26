76311af7cbaac89e8c974c096a5f0ec07ea17db2
prasad-bigdp <bigdp585@gmail.com>
2024-08-26 08:54:25 +0530

    update

---
+++ mainProject/product.js
@@ -0,0 +1,29 @@
+// To get the Id in the URL
+const params = new URLSearchParams(window.location.search);
+const pid = params.get('pid');
+console.log(pid)
+function fetchData ()
+{
+    fetch(`https://dummyjson.com/product/${pid}`)
+        .then((res) => res.json())
+        .then((data) => displayData(data))
+    .catch((err)=>console.log(err))
+}
+fetchData();
+const leftDiv = document.getElementById('left');
+const rightDiv= document.getElementById('right')
+const displayData = (p)=>{
+    const img = document.createElement('img');
+    img.src = p.thumbnail;
+    img.alt = p.title;
+    leftDiv.append(img);
+    const title = document.createElement('h2');
+    title.textContent = p.title
+    const desc = document.createElement('p');
+    desc.textContent = p.description
+    const price = document.createElement('p');
+    price.textContent= "Price: $"+ p.price
+    const addCartButton = document.createElement('button')
+    addCartButton.textContent = "Add Cart";
+    rightDiv.append(title,desc,price,addCartButton)
+}
---
+++ mainProject/script.js
@@ -0,0 +1,55 @@
+const productsListDiv = document.getElementById("productsList");
+const cat= document.getElementById('cat')
+const fetchCategories = () =>
+{
+    fetch("https://dummyjson.com/products/categories")
+        .then((res) => res.json())
+        .then((data) =>
+        {
+            data.forEach((data) =>
+            {
+                const op = document.createElement("option")
+				op.textContent = data.name
+				op.value = data.slug
+				cat.append(op)
+            })
+    })
+}
+fetchCategories();
+document.getElementById('cat').addEventListener('input', function (e)
+{
+    fetch(`https://dummyjson.com/products/category/${e.target.value}`)
+        .then((res) => res.json())
+    .then((data)=>displayData(data.products))
+})
+const fetchData = async () =>
+{
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+    displayData(data.products)
+}
+fetchData();
+function displayData (products)
+{
+    productsListDiv.textContent=''
+    products.forEach((pro) =>
+    {
+        const proDiv = document.createElement('div');
+        proDiv.classList.add('product')
+        const proImage = document.createElement('img');
+        proImage.src = pro.thumbnail;
+        proImage.alt = pro.title;
+        const proLink = document.createElement('a');
+        proLink.href = `./product.html?pid=${pro.id}`
+        proLink.target="_blank"
+        const proTitle = document.createElement('h2');
+        proTitle.textContent = pro.title
+        proLink.append(proTitle)
+        const proPrice = document.createElement('p');
+        proPrice.textContent= "Price: $"+pro.price
+        const addCartButton = document.createElement('button');
+        addCartButton.textContent= "Add Cart"
+        proDiv.append(proImage, proLink, proPrice, addCartButton);
+        productsListDiv.appendChild(proDiv)
+   })
+}
